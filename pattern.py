aef27e86d1dd8f64d7d9cd66551a822f10e90a41
SangayChozom <02230106.cst@rub.edu.bt>
2024-03-18 15:03:38 +0530

    Adding new file

---
+++ pattern.py
@@ -0,0 +1,7 @@
+height = int(input("Enter the height of the triangle(number of rows): "))
+i= 1
+print("Right triangle pattern: ")
+for i in range(1, height + 1):
+        for j in range(1, i + 1):
+            print("*", end = " ")
+        print()
