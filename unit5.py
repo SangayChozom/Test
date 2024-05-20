b1a393290c485cd32aa7d9751fa43cf0b5f7f87a
SangayChozom <02230106.cst@rub.edu.bt>
2024-05-20 14:09:12 +0530

    Adding new file

---
+++ unit5.py
@@ -0,0 +1,19 @@
+class Dog:
+    def __init__(self,name,colour,age):
+        self.name = name
+        self.colour = colour
+        self.age = age
+
+Dawa = Dog("Dawa", "Blue", 24)
+Nima = Dog("Nima", "Pink", 34)
+Blacky = Dog("Blacky", "White", 44)
+
+print(Dawa.name)
+print(Dawa.colour)
+print(Dawa.age)
+print(Nima.name)
+print(Nima.colour)
+print(Nima.age)
+print(Blacky.name)
+print(Blacky.colour)
+print(Blacky.age)
